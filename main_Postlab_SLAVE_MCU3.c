ec864e69f1ee4326ff8ff5d3394c8c5102c0bda2
Javier A. Pérez <91178478+JAPM8@users.noreply.github.com>
2022-05-13 21:57:23 -0600

    Add files via upload

---
+++ main_Postlab_SLAVE_MCU3.c
@@ -0,0 +1,115 @@
+/*
+ * File:   main_Postlab_SLAVE_MCU3.c
+ * Author: javyp
+ *
+ * Created on 13 de mayo de 2022, 11:59 AM
+ */
+
+// CONFIG1
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#include <stdint.h>
+/*------------------------------------------------------------------------------
+ * CONSTANTES 
+ ------------------------------------------------------------------------------*/
+#define _XTAL_FREQ 1000000
+
+/*------------------------------------------------------------------------------
+ * VARIABLES 
+ ------------------------------------------------------------------------------*/
+char cont_master = 0;
+char cont_slave = 0xFF;
+char val_temporal = 0;
+uint8_t contador = 0;
+/*------------------------------------------------------------------------------
+ * PROTOTIPO DE FUNCIONES 
+ ------------------------------------------------------------------------------*/
+void setup(void);
+/*------------------------------------------------------------------------------
+ * INTERRUPCIONES 
+ ------------------------------------------------------------------------------*/
+void __interrupt() isr (void){
+    if (PIR1bits.SSPIF){
+        
+        SSPBUF = contador; // respondemos con valor de contador
+        PIR1bits.SSPIF = 0;  // Limpiamos bandera de interrupción 
+    }
+    if (INTCONbits.RBIF){
+        if (!PORTBbits.RB0){ //Pb de incremento
+            contador ++;
+        } else if (!PORTBbits.RB1){ //Pb de decremento
+            contador --;
+        }       
+        INTCONbits.RBIF = 0; //Limpieza de bandera
+    }
+    return;
+}
+/*------------------------------------------------------------------------------
+ * CICLO PRINCIPAL
+ ------------------------------------------------------------------------------*/
+void main(void) {
+    setup();
+    while(1){        
+        // Envio y recepcion de datos en maestro
+    }
+    return;
+}
+/*------------------------------------------------------------------------------
+ * CONFIGURACION 
+ ------------------------------------------------------------------------------*/
+void setup(void){
+    ANSEL = 0;
+    ANSELH = 0;
+    
+    TRISBbits.TRISB0 = 1;      //RB0 & RB1 COMO INPUT
+    TRISBbits.TRISB1 = 1;
+    PORTB = 0;      //CLEAR DE PUERTO B
+    
+    TRISA = 0b0100000;
+    PORTA = 0;
+    
+    OSCCONbits.IRCF = 0b100;    // 1MHz
+    OSCCONbits.SCS = 1;         // Reloj interno
+    
+    OPTION_REGbits.nRBPU = 0; // Se habilitan pull-up de PORTB
+    WPUBbits.WPUB0 = 1; //Se habilita pull de RB0
+    WPUBbits.WPUB1 = 1; //Se habilita pull de RB1
+    
+    // Configuracion de SPI
+    // Configs del esclavo
+    TRISC = 0b00011000; // -> SDI y SCK entradas, SD0 como salida
+    PORTC = 0;
+
+    // SSPCON <5:0>
+    SSPCONbits.SSPM = 0b0100;   // -> SPI Esclavo, SS hablitado
+    SSPCONbits.CKP = 0;         // -> Reloj inactivo en 0
+    SSPCONbits.SSPEN = 1;       // -> Habilitamos pines de SPI
+    // SSPSTAT<7:6>
+    SSPSTATbits.CKE = 1;        // -> Dato enviado cada flanco de subida
+    SSPSTATbits.SMP = 0;        // -> Dato al final del pulso de reloj
+
+    PIR1bits.SSPIF = 0; // Limpiamos bandera de SPI
+    PIE1bits.SSPIE = 1;  // Habilitamos int. de SPI
+    INTCONbits.PEIE = 1;
+    INTCONbits.GIE = 1;
+    INTCONbits.RBIE = 1; //Se habilitan interrupciones del PORTB para asimilar un antirebote
+    IOCBbits.IOCB0 = 1;  //Se habilitan interrupción por cambio de estado de RB0
+    IOCBbits.IOCB1 = 1;  //Se habilitan interrupción por cambio de estado de RB1
+    INTCONbits.RBIF = 0; //Limpieza de bandera de interrupción de IOCB
+    
+}
